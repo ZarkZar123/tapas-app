3aef0ffecce9343e11a2ed251914744b20f07d36
Abdul-Razak Adams <abdulrazakadams28@gmail.com>
2024-03-04 17:38:11 +0000

    add handle user input funtionality

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function handlePlates (event){
+    event.preventDefault()
+    let plate = addItems.item.value
+    items.push(plate)
+    addItems.reset()
+    console.log(plate);
+}
+
+document.querySelector('[type=submit]').addEventListener('click',handlePlates)
