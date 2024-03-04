8346139e5f04d41a59155d28380ef706e9dda386
Abdul-Razak Adams <abdulrazakadams28@gmail.com>
2024-03-04 17:55:37 +0000

    add display list functionality

--- src/script.js
+++ src/script.js
@@ -1,13 +1,29 @@
 const addItems = document.querySelector(".add-items");
-const itemsList = document.querySelector(".plates");
-const items = [];
+const plateList = document.querySelector(".plates");
+const plates = [];
 
 function handlePlates (event){
     event.preventDefault()
     let plate = addItems.item.value
-    items.push(plate)
+    plates.push(plate)
     addItems.reset()
     console.log(plate);
+
+    handleDisplayList(plates)
 }
 
 document.querySelector('[type=submit]').addEventListener('click',handlePlates)
+
+function handleDisplayList(plates=[]){
+    plateList.innerHTML= plates.map((plate,i)=>{
+            return `
+            <li>
+                <input type='checkbox' id='item${i}' checked />
+                <label for='item${i}'>
+                    <span>${plate}</span>
+                </label>
+                <button class='delete'> ❌ </button>
+            </li>
+            `
+    })
+}
