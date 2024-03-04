24df18d3d8052deb95c9260e9274129592d2f98a
Abdul-Razak Adams <abdulrazakadams28@gmail.com>
2024-03-04 18:27:11 +0000

    add toggle done function

--- src/script.js
+++ src/script.js
@@ -4,26 +4,48 @@ const plates = [];
 
 function handlePlates (event){
     event.preventDefault()
+    // get user input and add to list
     let plate = addItems.item.value
-    plates.push(plate)
+    plates.push({text:plate,done:false})
     addItems.reset()
-    console.log(plate);
 
+    // store to local storage
+    localStorage.setItem('plates', JSON.stringify(plates))
     handleDisplayList(plates)
 }
 
-document.querySelector('[type=submit]').addEventListener('click',handlePlates)
+
 
 function handleDisplayList(plates=[]){
     plateList.innerHTML= plates.map((plate,i)=>{
             return `
             <li>
-                <input type='checkbox' id='item${i}' checked />
+                <input onclick='toggleDone(event)' type='checkbox' id='item${i}' data-index=${i} ${plate.done ? "checked": ""} />
                 <label for='item${i}'>
-                    <span>${plate}</span>
+                    <span>${plate.text}</span>
                 </label>
                 <button class='delete'> ❌ </button>
             </li>
             `
-    })
+    }).join("")
 }
+
+function toggleDone(event){
+    const element =event.target
+   const plateIndex = element.dataset.index
+   console.log('heloo');
+   plates[plateIndex].done = !plates[plateIndex].done
+    console.log(plates);
+   localStorage.setItem('plates', JSON.stringify(plates))
+    handleDisplayList(plates)
+
+}
+
+(function init(){
+   const data = localStorage.getItem('plates')
+   data && plates.push(...JSON.parse(data))
+   handleDisplayList(plates)
+
+})()
+
+document.querySelector('[type=submit]').addEventListener('click',handlePlates)
